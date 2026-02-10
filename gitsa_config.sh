912dce53dc79a4eb06269b981ac6dc2c663b58bd
Chanraksa Ory <OCraksa@gmail.com>
2026-02-10 09:57:48 +0700

    removed file

--- gitsa_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-TELEGRAM_BOT_TOKEN="8391471887:AAFXKv2xqmfui3ngfnkov93JBVtsVt-FrAY"
-SUMMARY_AI_KEY="AIzaSyAh8QICC7cFm94KJqUj2vWYg3GK_9Uca0c"
-CHAT_ID="-4894760886"
-
-REMOTE_NAMES=("origin" "org")
-REMOTE_URLS=("https://github.com/RaksaOC/KOMPLEX.git" "https://github.com/KOMPLEX-KH/KOMPLEX.git")
-
-ALLOW_ONE_MATCH="false"
